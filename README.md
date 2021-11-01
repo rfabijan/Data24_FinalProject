c676229cbb0743973a02cbad4589a968755e50e1
aremansparta <91061773+aremansparta@users.noreply.github.com>
2021-11-01 17:21:39 +0000

    Downloaded cloud files to local machine

--- README.md
+++ README.md
@@ -3,23 +3,29 @@ The repo for Data24 for their final project <br>
 https://spartaglobal.atlassian.net/jira/software/projects/DATA24/boards/300/backlog
 
 
-Ahmed
-Link to Jira Board:
+Link to Jira Board:<br/>
 https://spartaglobal.atlassian.net/jira/software/projects/DATA24/boards/300/backlog
 
+
 Roles:
-Tia – Scrum Master
-Rob – Dev, Git baby
-Sully – Dev
-Marcel – Test
-Matthew R– Test
-Matt L - BA, Dev
-Ahmed – Test, BA
+- Tia – Scrum Master
+- Rob – Dev, Git baby
+- Sully – Dev
+- Marcel – Test
+- Matthew R– Test
+- Matt L - BA, Dev
+- Ahmed – Test, BA
+
 
 Schedule:
-09:30 – 09:50: Sprint Planning
-09:50 – 12:30: Sprint
-13:30 – 13:45: Scrum Meeting/Standup
-13:45 – 16:15 – Sprint 2
-16:15 – 16:30: Sprint Review
-16:30 – 17:00: Retrospective/review with Paula
+- 09:30 – 09:50: Sprint Planning
+- 09:50 – 12:30: Sprint
+- 13:30 – 13:45: Scrum Meeting/Standup
+- 13:45 – 16:15 – Sprint 2
+- 16:15 – 16:30: Sprint Review
+- 16:30 – 17:00: Retrospective/review with Paula
+
+
+Root File Structure:<br/>
+datafiles -> Contains a copy of all the raw data files
+pipeline -> Contains the app and test package
---
+++ pipeline/scripts/s3_extractor.py
@@ -0,0 +1,42 @@
+"""
+This script:
+1. Makes a local copy of all the S3 object files (in the 'data24-final-project' bucket)
+
+"""
+
+import boto3
+import botocore
+import os
+from pprint import pprint as pp
+
+client = boto3.client('s3')
+resource = boto3.resource('s3')
+bucket = resource.Bucket('data24-final-project')
+objects = [obj.key for obj in bucket.objects.all()]  # All the files/objects in the S3 bucket
+
+total_files = len(objects)
+current_count = 0
+
+# Download all the objects
+for obj in objects:
+    print(f'Downloading {current_count}/{total_files} ({(current_count/total_files)*100})')
+
+    try:
+        filename = obj.replace('/', '-')
+        filename = filename.replace(' ', '_')
+        local_path = f'../../datafiles/{filename}'
+        bucket.download_file(obj, local_path)
+
+    # For when we can't download the file
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+    # If the destination directory does not exist, create it and try agaibn
+    except FileNotFoundError as e:
+        os.makedirs('../../datafiles')
+        bucket.download_file(obj, local_path)
+
+    current_count += 1
