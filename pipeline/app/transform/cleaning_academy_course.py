26289998693e69866d39e40483fb8981a26adbc6
Marcel k <mkrzyzanowski@spartaglobal.com>
2021-11-04 12:24:57 +0000

    updated documentation

--- pipeline/app/transform/cleaning_academy_course.py
+++ pipeline/app/transform/cleaning_academy_course.py
@@ -1,11 +1,11 @@
-
+# str as input(name), output the name as str in a tuple format
 def clean_name(name):
     pass
 
-
+# str as input(trainer_name), output the name as str in a tuple format, ("Bruce Lugo") would output ("Bruce", "Lugo")
 def clean_trainer(trainer_name):
     pass
 
-
+# not specified yet
 def clean_skill_value():
     pass
