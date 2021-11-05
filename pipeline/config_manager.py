fef8842cb2d77e81f75b03432a7a3977d8e1aca4
aremansparta <91061773+aremansparta@users.noreply.github.com>
2021-11-05 13:45:07 +0000

    Started working on Load section
    This commit contains the following:
    1. Update to the config.ini file to include SQL related configs
    2. Update to config_manager.py to include the newly added configurations
    3. Update to ERD diagram. A few field names were changed as they were previously SQL reserved keywords
    4. Created an .sql script to create the required database from scratch
    
    Co-Authored-By: MatthewReddy0 <91061960+MatthewReddy0@users.noreply.github.com>
    Co-Authored-By: Marcel k. <91061923+marcelk98@users.noreply.github.com>

--- pipeline/config_manager.py
+++ pipeline/config_manager.py
@@ -3,8 +3,17 @@ import configparser
 import os
 import pytest
 
-_config = configparser.ConfigParser()
+config_files = [
+    os.path.join(PROJECT_ROOT_DIR, 'config.ini'),
+    os.path.join(PROJECT_ROOT_DIR, 'private_config.ini')
+]
 
-_config.read(os.path.join(PROJECT_ROOT_DIR, 'config.ini'))
+_config = configparser.ConfigParser()
+_config.read(config_files)
 
 BUCKET_NAME = _config['default']['bucket_name']
+
+DB_SERVER = _config['SQL']['server']
+DB_NAME = _config['SQL']['database']
+DB_USERNAME = _config['SQL']['username']
+DB_PASSWORD = _config['SQL']['password']
