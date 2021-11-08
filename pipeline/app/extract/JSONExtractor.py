73b50252e11d21e5143812db1922880b4286dd5c
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-11-08 10:27:21 +0000

    Update JSONExtractor

--- pipeline/app/extract/JSONExtractor.py
+++ pipeline/app/extract/JSONExtractor.py
@@ -20,53 +20,64 @@ class JSONExtractor(s3c.S3ParentClass):
         jsonfile = json.load(self.client.get_object(Bucket=self.bucket_name, Key=key)["Body"])
         return jsonfile
 
+
     # Returns a name if exists as a key in dictonary
-    def extract_name(self, json_file) -> str:
+    @staticmethod
+    def extract_name(json_file) -> str:
         if "name" in json_file.keys():
             return json_file["name"]
         else:
             return None
 
     # Returns a date if exists as a key in dictonary
-    def extract_date(self, json_file) -> str:
+    @staticmethod
+    def extract_date(json_file) -> str:
         if "date" in json_file.keys():
             return json_file["date"]
         else:
             return None
 
     # Returns a tech_self_score
-    def extract_tech_self_score(self, json_file) -> dict:
+    @staticmethod
+    def extract_tech_self_score(json_file) -> dict:
         if "tech_self_score" in json_file.keys():
             return json_file["tech_self_score"]
         else:
             return None
 
     # Returns a strengths
-    def extract_strengths(self, json_file) -> list:
+    @staticmethod
+    def extract_strengths(json_file) -> list:
         return json_file["strengths"]
 
     # Returns a weaknesses
-    def extract_weaknesses(self, json_file) -> list:
+    @staticmethod
+    def extract_weaknesses(json_file) -> list:
         return json_file["weaknesses"]
 
     # Returns a self_development
-    def extract_self_development(self, json_file) -> str:
+    @staticmethod
+    def extract_self_development(json_file) -> str:
         return json_file["self_development"]
 
     # Returns a geo_flex
-    def extract_geo_flex(self, json_file) -> str:
+    @staticmethod
+    def extract_geo_flex(json_file) -> str:
         return json_file["geo_flex"]
 
     # Returns a financial_support_self
-    def extract_financial_support_self(self, json_file) -> str:
+    @staticmethod
+    def extract_financial_support_self(json_file) -> str:
         return json_file["financial_support_self"]
 
     # Returns a result
-    def extract_result(self, json_file) -> str:
+    @staticmethod
+    def extract_result(json_file) -> str:
         return json_file["result"]
 
     # Returns a course_interest
-    def extract_course_interest(self, json_file) -> str:
+    @staticmethod
+    def extract_course_interest(json_file) -> str:
         return json_file["course_interest"]
 
 
