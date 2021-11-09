394b112e9aa617d74f47276bac695ac1ac0cf8e5
Tia <tiaedmead@Tias-MacBook-Pro.local>
2021-11-09 14:46:11 +0000

    Update cleaning_academy_course.py

--- pipeline/app/transform/cleaning_academy_course.py
+++ pipeline/app/transform/cleaning_academy_course.py
@@ -1,11 +1,94 @@
-# str as input(name), output the name as str in a tuple format
-def clean_name(name):
-    pass
+import pipeline.app.extract.csv_extractor as extractor
 
-# str as input(trainer_name), output the name as str in a tuple format, ("Bruce Lugo") would output ("Bruce", "Lugo")
-def clean_trainer(trainer_name):
-    pass
 
-# not specified yet
-def clean_skill_value():
-    pass
+class AcademyCleaner(extractor.AcademiesCsvExtractor):
+    def __init__(self):
+        super(AcademyCleaner, self).__init__()
+        self.__error_names = set()
+        self.__error_trainer_names = set()
+
+    @property
+    def error_names(self):
+        return self.__error_names
+
+    @property
+    def error_trainer_names(self):
+        return self.__error_trainer_names
+
+    def clean_name(self, name: str) -> tuple:
+        name = name.title()
+        if name.count(" ") > 1 or "-" in name:
+            self.error_names.add(name)
+        if " " in name:
+            space_index = name.index(" ")
+            name_list = [name[0:space_index], name[space_index + 1:]]
+            return name_list[0], name_list[1]
+        else:
+            print(name)
+
+    def clean_trainer(self, trainer_name: str) -> tuple:
+        trainer_name = trainer_name.title()
+        if trainer_name.count(" ") > 1 or "-" in trainer_name:
+            self.error_names.add(trainer_name)
+        if " " in trainer_name:
+            space_index = trainer_name.index(" ")
+            name_list = [trainer_name[0:space_index], trainer_name[space_index + 1:]]
+            return name_list[0], name_list[1]
+        else:
+            print(trainer_name)
+
+    @staticmethod
+    def clean_skill_value(score: int) -> int or None:
+        if score in range(1, 9):
+            return score
+
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    test = AcademyCleaner()
+    print(test.clean_name("Rossie Caitlin"))
+
+
+# def clean_name(name):
+#     name_errors = []
+#     name = name.title()                                                     # capitalises the first letter of each word
+#
+#     if name.count(" ") > 1 or "-" in name:                                  # checks for multiple spaces and hyphens
+#         name_errors.append(name)                                            # adds names with errors to the error list
+#         return f"These names contain errors: {name_errors}"                 # returns the names with errors
+#
+#     if " " in name:                                                         # checks for a space in the name
+#         space_index = name.index(" ")                                       # gets the index number of the space
+#         list_of_names = [name[0: space_index], name[space_index + 1:]]      # separates first and last name
+#         return list_of_names[0], list_of_names[1]                           # returns first and last name as a tuple
+#
+#     else:
+#         return name
+#
+#
+# def clean_trainer(trainer_name):
+#     trainer_name_errors = []
+#     trainer_name = trainer_name.title()
+#
+#     if trainer_name.count(" ") > 1 or "-" in trainer_name:
+#         trainer_name_errors.append(trainer_name)
+#         return f"These names contain errors: {trainer_name_errors}"
+#
+#     if " " in trainer_name:
+#         space_index = trainer_name.index(" ")
+#         list_of_trainer_names = [trainer_name[0: space_index], trainer_name[space_index + 1:]]
+#         return list_of_trainer_names[0], list_of_trainer_names[1]
+#
+#     else:
+#         return trainer_name
+#
+#
+# def clean_skill_value(score):
+#     if score in range(1, 9):
+#         return score
+#
+#     else:
+#         return None
+
