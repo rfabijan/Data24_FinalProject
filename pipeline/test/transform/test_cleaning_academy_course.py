b8326f5de38d95d9e64466d95ead7f162365d43a
Marcel k <mkrzyzanowski@spartaglobal.com>
2021-11-09 16:46:51 +0000

    end of the day test code update
    end of the day test code update

--- pipeline/test/transform/test_cleaning_academy_course.py
+++ pipeline/test/transform/test_cleaning_academy_course.py
@@ -1,20 +1,24 @@
 from pipeline.app.transform import cleaning_academy_course as clean
 
+output = clean.AcademyCleaner()
 
 def test_clean_name():
-    testy = clean.AcademyCleaner()
-    new_value = testy.clean_name("Rossie Caitlin")
-    assert testy.clean_name("Rossie Caitlin") == ("Rossie", "Caitlin")
+    new_value = output.clean_name("Rossie Caitlin")
+    assert new_value == ("Rossie", "Caitlin")
     assert type(new_value) is tuple
 
 
 def test_clean_trainer():
-    testy = clean.AcademyCleaner()
-    new_value = testy.clean_trainer("Bruce Lugo")
-    assert testy.clean_trainer("Bruce Lugo") == ("Bruce", "Lugo")
+    new_value = output.clean_trainer("Bruce Lugo")
+    assert new_value == ("Bruce", "Lugo")
     assert type(new_value) is tuple
 
 
-# not finalised
 def test_clean_skill_value():
-    pass
+    new_value = output.clean_skill_value(3)
+    assert new_value == 3
+    assert type(new_value) is int
+    assert new_value in range(1,9)
+
+
+# have not tested the error_name functions because they return empty sets
