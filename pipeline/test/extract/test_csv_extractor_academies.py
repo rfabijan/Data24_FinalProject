1e5714d03977c2128357a51f202401e43bd6e6fc
MatthewReddy0 <MReddy@spartaglobal.com>
2021-11-10 14:01:25 +0000

    Fixed csv extraction tests
    Fixed csv extraction tests

--- pipeline/test/extract/test_csv_extractor_academies.py
+++ pipeline/test/extract/test_csv_extractor_academies.py
@@ -11,8 +11,8 @@ def test_len_of_rows():
     assert type(output) is int
 
 
-def test_extract_name():
-    output = values.extract_name(values.single_csv("Academy/Data_28_2019-02-18.csv"), 3)
+def test_extract_csv_name():
+    output = values.extract_csv_name(values.single_csv("Academy/Data_28_2019-02-18.csv"), 3)
     assert type(output) is str
     assert 'Aida Bothams' in output
     assert 'aida Bothams' not in output
@@ -20,8 +20,8 @@ def test_extract_name():
     assert 'aida bothams' not in output
 
 
-def test_extract_trainer():
-    output = values.extract_trainer(values.single_csv("Academy/Business_20_2019-02-11.csv"), 4)
+def test_extract_academies_trainer():
+    output = values.extract_academies_trainer(values.single_csv("Academy/Business_20_2019-02-11.csv"), 4)
     assert type(output) is str
     assert 'Gregor Gomez' in output
     assert 'Gregor gomez' not in output
@@ -29,13 +29,13 @@ def test_extract_trainer():
     assert 'gregor gomez' not in output
 
 
-def test_extract_skill_value():
-    output = values.extract_skill_value(values.single_csv("Academy/Data_31_2019-05-20.csv"), 'Determined_W1', 1)
+def test_extract_academies_skill_value():
+    output = values.extract_academies_skill_value(values.single_csv("Academy/Data_31_2019-05-20.csv"), 'Determined_W1', 1)
     assert output >= 7 or output <= 7
     assert type(output) == np.int64
 
 
-def test_singe_csv():
+def test_single_csv():
     output = values.single_csv('Academy/Engineering_21_2019-07-15.csv')
     output2 = output['name'][1]
     output3 = output['trainer'][5]
@@ -48,21 +48,21 @@ def test_singe_csv():
     assert type(output) == pd.core.frame.DataFrame
 
 
-def test_extract_skill_values_per_person_per_week():
-    output = values.extract_skill_values_per_person_per_week(values.single_csv('Academy/Business_29_2019-11-18.csv'))
+def test_extract_academies_skill_values_per_person_per_week():
+    output = values.extract_academies_skill_values_per_person_per_week(values.single_csv('Academy/Business_29_2019-11-18.csv'))
     assert type(output) is dict
     assert len(output) == 10
     assert 'Rossie Caitlin' in output
 
 
-def test_extract_course_name():
-    output = values.extract_course_name('Academy/Engineering_29_2019-12-30.csv')
+def test_extract_academies_course_name():
+    output = values.extract_academies_course_name('Academy/Engineering_29_2019-12-30.csv')
     assert type(output) is str
     assert 'Engineering 29' in output
 
 
-def test_extract_date():
-    output = values.extract_date('Academy/Business_27_2019-09-16.csv')
+def test_extract_academies_date():
+    output = values.extract_academies_date('Academy/Business_27_2019-09-16.csv')
     assert type(output) is str
     assert '2019-09-16' in output
     assert output[0:4] == '2019'
