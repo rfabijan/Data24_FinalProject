bad7bf2483b3a4209becef5e790ce950f56a5be4
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-11-11 16:27:05 +0000

    UPDATE __main__ file

--- __main__.py
+++ __main__.py
@@ -1,51 +1,51 @@
 import pipeline.config_manager as conf
-from pipeline.app.transform.cleaning_talent import JsonCleaner
-from pipeline.app.transform.cleaning_academy_course import AcademyCleaner
-from pipeline.app.transform.cleaning_talent_applicants import Applicants_Cleaner
-from pipeline.app.transform.cleaning_talent_sparta_day import TxtCleaner
 from pipeline.app.load.database_creator import DatabaseCreator
+from pipeline.app.load.pre_load_formatter import PreLoadFormatter
+from pipeline.app.load.insert_data import *
+from pipeline.app.load.convert_id_columns import *
 
 if __name__ == "__main__":
 
-    print("Connecting to S3...")
-    # Receiving data from S3 and cleaning
-    clean_json = JsonCleaner()
-    clean_academy = AcademyCleaner()
-    clean_applicants = Applicants_Cleaner()
-    clean_txt = TxtCleaner()
-    print("CONNECTION ESTABLISHED!")
+    formatter = PreLoadFormatter()
 
-
-    print("\nFILE EXTRACTION!")
-    json_keys = clean_json.extract_json_keys
-    print("JSON file extraction ...")
-    for json in json_keys:
-        file = clean_json.pull_single_json(json)
-    print("JSON file extraction done!\n")
-
-    applicants_keys = clean_applicants.keys
-    print("Applicants CSV file extraction ...")
-    for app in applicants_keys:
-        file = clean_applicants.single_csv(app)
-    print("Applicants CSV file extraction done!\n")
-
-    academy_keys = clean_academy.keys
-    print("Academy CSV file extraction ...")
-    for academy in applicants_keys:
-        file = clean_academy.single_csv(academy)
-    print("Academy CSV file extraction done!\n")
-
-    txt_keys = clean_txt.txt_keys
-    print("TXT file extraction ...")
-    for txt in txt_keys:
-        file = clean_txt.pull_text_object_as_list(txt)
-    print("TXT file extraction done!\n")
+    sparta_day_df = ci_app_sparta_day(formatter)
+    course_trainer_jt_df = ci_course_trainer_jt(formatter)
+    applicants_df = ci_applicants(formatter)
+    app_sparta_day_df = ci_app_sparta_day(formatter)
+    tech_self_score_jt_df = ci_tech_self_score_jt(formatter)
+    app_strengths_jt_df = ci_app_strengths_jt(formatter)
+    app_weaknesses_jt_df = ci_app_weaknesses_jt(formatter)
+    spartans_df = ci_spartans(formatter)
+    tracker_df = ci_tracker_jt(formatter)
 
     # Database creation stage
     database = DatabaseCreator()
     database.reset_database()
     database.create_database()
     database.run_script(conf.SQL_SCRIPT)
+
+    insert(formatter.academy_df, "Academy")
+    insert(formatter.address_df, "Addresses")
+    insert(applicants_df, "Applicants")
+    insert(app_sparta_day_df , "ApplicantSpartaDay")
+    insert(app_strengths_jt_df , "ApplicantStrengths")
+    insert(app_weaknesses_jt_df, "ApplicantWeaknesses")
+    insert(formatter.core_skills_df, "CoreSkills")
+    insert(formatter.course_df, "Course")
+    insert(course_trainer_jt_df, "CourseTrainer")
+    insert(formatter.invitors_df, "Invitors")
+    insert(sparta_day_df, "SpartaDay")
+    insert(spartans_df, "Spartans")
+    insert(formatter.streams_df, "Streams")
+    insert(formatter.strengths_df, "Strengths")
+    insert(formatter.tech_skills_df, "TechSkill")
+    insert(tech_self_score_jt_df, "TechSelfScore")
+    insert(tracker_df, "Tracker")
+    insert(formatter.trainer_df, "Trainer")
+    insert(formatter.weakness_df, "Weaknesses")
+
+
+
 
     # print(clean_json.extract_json_keys)
     # print(clean_academy.keys)
