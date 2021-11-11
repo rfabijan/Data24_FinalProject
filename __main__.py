838ece7e16aba328309aba7bb4b728717cc1da83
MatthewReddy0 <MReddy@spartaglobal.com>
2021-11-11 21:37:41 +0000

    push to sully's branch

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ from pipeline.app.load.database_creator import DatabaseCreator
 
 if __name__ == "__main__":
 
+<<<<<<< Updated upstream
     print("Connecting to S3...")
     # Receiving data from S3 and cleaning
     clean_json = JsonCleaner()
@@ -14,6 +15,46 @@ if __name__ == "__main__":
     clean_applicants = Applicants_Cleaner()
     clean_txt = TxtCleaner()
     print("CONNECTION ESTABLISHED!")
+=======
+    formatter = PreLoadFormatter()
+
+    sparta_day_df = ci_app_sparta_day(formatter)
+    course_trainer_jt_df = ci_course_trainer_jt(formatter)
+    applicants_df = ci_applicants(formatter)
+    app_sparta_day_df = ci_app_sparta_day(formatter)
+    tech_self_score_jt_df = ci_tech_self_score_jt(formatter)
+    app_strengths_jt_df = ci_app_strengths_jt(formatter)
+    app_weaknesses_jt_df = ci_app_weaknesses_jt(formatter)
+    spartans_df = ci_spartans(formatter)
+    tracker_df = ci_tracker_jt(formatter)
+    print(applicants_df.columns)
+
+    # Database creation stage
+    database = DatabaseCreator()
+    database.reset_database()
+    database.create_database()
+    database.run_script(conf.SQL_SCRIPT)
+
+    insert_df(formatter.academy_df, "Academy", database.cursor, conf.DB_NAME)
+    insert_df(formatter.address_df, "Addresses", database.cursor, conf.DB_NAME)
+    insert_df(applicants_df, "Applicants", database.cursor, conf.DB_NAME)
+    insert_df(app_sparta_day_df , "ApplicantSpartaDay", database.cursor, conf.DB_NAME)
+    insert_df(app_strengths_jt_df , "ApplicantStrengths", database.cursor, conf.DB_NAME)
+    insert_df(app_weaknesses_jt_df, "ApplicantWeaknesses", database.cursor, conf.DB_NAME)
+    insert_df(formatter.core_skills_df, "CoreSkills", database.cursor, conf.DB_NAME)
+    insert_df(formatter.course_df, "Course", database.cursor, conf.DB_NAME)
+    insert_df(course_trainer_jt_df, "CourseTrainer", database.cursor, conf.DB_NAME)
+    insert_df(formatter.invitors_df, "Invitors", database.cursor, conf.DB_NAME)
+    insert_df(sparta_day_df, "SpartaDay", database.cursor, conf.DB_NAME)
+    insert_df(spartans_df, "Spartans", database.cursor, conf.DB_NAME)
+    insert_df(formatter.streams_df, "Streams", database.cursor, conf.DB_NAME)
+    insert_df(formatter.strengths_df, "Strengths", database.cursor, conf.DB_NAME)
+    insert_df(formatter.tech_skills_df, "TechSkill", database.cursor, conf.DB_NAME)
+    insert_df(tech_self_score_jt_df, "TechSelfScore", database.cursor, conf.DB_NAME)
+    insert_df(tracker_df, "Tracker", database.cursor, conf.DB_NAME)
+    insert_df(formatter.trainer_df, "Trainer", database.cursor, conf.DB_NAME)
+    insert_df(formatter.weakness_df, "Weaknesses", database.cursor, conf.DB_NAME)
+>>>>>>> Stashed changes
 
     print("\nFILE EXTRACTION!")
     json_keys = clean_json.extract_json_keys
--- pipeline/app/load/insert_data.py
+++ pipeline/app/load/insert_data.py
@@ -1,14 +1,41 @@
 import pyodbc
 import pipeline.config_manager as conf
+<<<<<<< Updated upstream
 
 
 # TODO: Validation on method parameters
 
+=======
+import urllib
+import sqlalchemy
+import pandas as pd
+# TODO: Validation on method parameters
+
+driver = "ODBC Driver 17 for SQL Server"
+database = conf.DB_NAME
+password = 'Passw0rd2018'
+server = 'localhost,1433'
+username = 'SA'
+params = urllib.parse.quote_plus(
+    'Driver=%s;' % driver +
+    'Server=tcp:%s,1433;' % server +
+    'Database=%s;' % database +
+    'Uid=%s;' % username +
+    'Pwd={%s};' % password +
+    'Encrypt=yes;' +
+    'TrustServerCertificate=yes;' +
+    'Connection Timeout=30;')
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine = sqlalchemy.create_engine(conn_str, pool_pre_ping=True)
+
+>>>>>>> Stashed changes
 
 def insert_data_df(df, tablename, connection, identity_insert_on_sql, identity_insert_off_sql):
-    connection.cursor.execute(identity_insert_on_sql)  # Allows us to insert ID rows
-    df.to_sql(tablename, connection, if_exists='append', index=False, index_label=None)
-    connection.cursor.execute(identity_insert_off_sql)  # Reverts the ID rows allowance to close the fabric of spacetime
+    connection.execute(identity_insert_on_sql)  # Allows us to insert ID rows
+    # df.to_sql(tablename, connection, if_exists='append', index=False, index_label=None, method='multi')
+    for each_item in df.index:
+        df.loc[each_item].to_sql(name=each_item, con=connection, if_exists='append', index=False)
+    connection.execute(identity_insert_off_sql)  # Reverts the ID rows allowance to close the fabric of spacetime
     connection.commit()
 
 
@@ -16,14 +43,28 @@ def insert():
     """
     This bit connects to the database
     """
+<<<<<<< Updated upstream
     server = 'localhost,1433'
     database = conf.DB_NAME
     username = 'SA'
     password = 'Passw0rd2018'
+=======
+    # server = 'localhost,1433'
+    # database = conf.DB_NAME
+    # username = 'SA'
+    # password = 'Passw0rd2018'
+    #
+    # data24etl_db = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID='
+    #                               + username + ';PWD=' + password)
+
+    identity_insert_on_sql = f"SET IDENTITY_INSERT {tablename} ON"
+    identity_insert_off_sql = f"SET IDENTITY_INSERT {tablename} OFF"
+>>>>>>> Stashed changes
 
     data24etl_db = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID='
                                   + username + ';PWD=' + password)
 
+<<<<<<< Updated upstream
     # TODO: The number of columns in each table in the dictionary below needs to be reduced because it currently counts
     #  ID columns, which aren't in the data tables
     # Key = table name, Value = Number of columns
@@ -46,10 +87,40 @@ def insert():
 
         # TODO: Alternative dataframe function usage
         insert_data_df(df, tablename, data24etl_db, identity_insert_on_sql, identity_insert_off_sql)
+=======
+"""
+def insert_df(df: pd.DataFrame, tablename, cursor, db_name='Data24ETL'):
+    values = ''
+    for index, row in df.iterrows():
+        inner_value = '('
+        for column in row:
+            inner_value += f"'{column}' "
+        inner_value = inner_value.rstrip()
+        inner_value = inner_value.replace(' ', ', ')
+        inner_value += ') '
+        values += inner_value
 
+    values = values.rstrip()
+    values = values.replace(') ', '), ')
 
-"""records_rows is a list of tuples (or lists, I don't think it matters), with each tuple being a row in the database"""
-# records_rows = [(a, b),
-#                 (a, b),
-#                 (a, b),
-#                 (a, b)]
+    query = f"INSERT INTO [{db_name}].[dbo].[{tablename}] VALUES {values};"
+    #print(query)
+    cursor.execute(query)
+"""
+
+>>>>>>> Stashed changes
+
+def insert_df(df: pd.DataFrame, tablename, cursor, db_name='Data24ETL'):
+    for index, row in df.iterrows():
+        inner_value = '('
+        for column in row:
+            inner_value += f"'{column}' "
+        inner_value = inner_value.rstrip()
+        inner_value = inner_value.replace(' ', ', ')
+        inner_value += ') '
+        query = f"""INSERT INTO [{db_name}].[dbo].[{tablename}] VALUES {inner_value};"""
+        cursor.execute(query)
+
+# if __name__ == "__main__":
+#     df = pd.DataFrame({"AcademyName": ["London", "Birmingham"]})
+#     insert_df(df=df, tablename='Academy', cursor=cursor, db_name='Data24ETL')
